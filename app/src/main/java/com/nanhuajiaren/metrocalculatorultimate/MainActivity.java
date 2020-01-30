48a78438bad4394db11816b3d1af2d6dd76ee712
nanhuajiaren <329857448@qq.com>
2020-01-30 10:26:39 +0800

    Initial commit

---
+++ app/src/main/java/com/nanhuajiaren/metrocalculatorultimate/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nanhuajiaren.metrocalculatorultimate;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //By:小杰哥哥
+    }
+}
